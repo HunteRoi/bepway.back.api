a3c43f743967b9fd64a8f3c7b60d7b1edc13ef5e
HunteRoi <etu33784@henallux.be>
2018-12-16 23:57:49 +0100

    Context null value now throw appropriate exception

--- app/API/Controllers/APIController.cs
+++ app/API/Controllers/APIController.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -6,7 +7,11 @@ namespace API.Controllers
 {
     public class APIController : ControllerBase
     {
-        public DbContext Context { get; set; }
+        private DbContext context;
+        public DbContext Context { 
+            get => context;
+            set => context = value ?? throw new ArgumentNullException(nameof(value));
+        }
         //public ILogger Logger { get; set; }
     }
 }
