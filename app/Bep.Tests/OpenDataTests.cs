9ecd2e0fdd61fe2b10d9a5a5df4488f8f1e9e91d
HunteRoi <etu33784@henallux.be>
2018-12-22 11:18:04 +0100

    I'm going to insert ODA in ODT

--- app/Bep.Tests/OpenDataTests.cs
+++ app/Bep.Tests/OpenDataTests.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+
+//using DAL;
+
 namespace Bep.Tests
 {
     [TestClass]
@@ -13,14 +15,14 @@ namespace Bep.Tests
         [TestMethod]
         public async Task GetWorks()
         {
-             var client = new HttpClient();
+            var client = new HttpClient();
 
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             var uri=new Uri("https://data.bep.be/api/records/1.0/search/?dataset=societes-de-nos-parcs-dactivite&rows=100&refine.nomparc=Parc+d%27activité+économique+de+Ciney+-+Biron+-+Lienne");
-            
+
             var response = await client.GetAsync(uri);
-            // Console.WriteLine(response);
+            Console.WriteLine(response);
             Assert.IsTrue(response.IsSuccessStatusCode, $"L'appel a échoué: {response.StatusCode}");
             //Assert.IsNotNull(response);
         }
