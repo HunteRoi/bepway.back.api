677e702590410bb93f754e4ebae187901118d568
HunteRoi <etu33784@henallux.be>
2019-01-02 20:39:54 +0100

    Change of metrics because they have their surface in fuck*ng m²

--- app/API/Infrastructure/DoubleConverter.cs
+++ app/API/Infrastructure/DoubleConverter.cs
@@ -4,11 +4,13 @@ namespace API.Infrastructure
 {
     public class StringDoubleConverter : IValueConverter<double, string>
     {
-        public string Convert(double source, ResolutionContext context) => source.ToString();
+        public string Convert(double source, ResolutionContext context) => (source*100).ToString();
+        // multiplicated by 100 because DB contains data in SQUARE METERS but display is in ARES
     }
 
     public class DoubleConverter : IValueConverter<string, double>
     {
-        public double Convert(string source, ResolutionContext context) => double.Parse(source, System.Globalization.CultureInfo.InvariantCulture);
+        public double Convert(string source, ResolutionContext context) => (double.Parse(source, System.Globalization.CultureInfo.InvariantCulture)/100);
+        // divided by 100 because display is in ARES, not in SQUARE METERS
     }
 }
